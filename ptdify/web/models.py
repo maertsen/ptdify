ec7eedeed4845fa21903d34679c784c41f99bc32
Maarten Aertsen <m.aertsen@visnescire.net>
2010-11-06 01:34:40 +0100

    First try at models

--- ptdify/web/models.py
+++ ptdify/web/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Context(models.Model):
+    user = models.ForeignKey(User)
+    name = models.CharField(max_length=50)
+    order = models.PositiveIntegerField(blank=True,null=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Project(models.Model):
+    user = models.ForeignKey(User)
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    defaultContext = models.ForeignKey(Context)
+    order = models.PositiveIntegerField(blank=True,null=True)
+
+    def __unicode__(self):
+        return self.name
+
+class Action(models.Model):
+    user = models.ForeignKey(User)
+    description = models.CharField(max_length=100)
+    notes = models.TextField(blank=True)
+    context = models.ForeignKey(Context)
+    project = models.ForeignKey(Project,blank=True,null=True)
+    due = models.DateField(blank=True,null=True)
+    showFrom = models.DateField(blank=True,null=True)
+    completed = models.BooleanField()
+
+    def __unicode__(self):
+        return self.description
+
