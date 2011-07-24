7f4000314eea12c592e10677b5193bc5d3fd080e
Maarten Aertsen <m.aertsen@visnescire.net>
2011-07-24 21:15:06 +0200

    Integrated authentication in generic list views

--- ptdify/web/urls.py
+++ ptdify/web/urls.py
@@ -1,26 +1,11 @@
 from django.conf.urls.defaults import *
 from django.views.generic import ListView
-from web.models import Action, Area, Context, Project
+from web.views import *
 
 urlpatterns = patterns('',
-    url(r'^$',
-        ListView.as_view(
-            queryset=Action.objects.all()[:5],
-            context_object_name='latest_action_list')),
-    url(r'^view/actions$',
-        ListView.as_view(
-            queryset=Action.objects.all()),
-        name="action_list"),
-    url(r'^view/areas$',
-        ListView.as_view(
-            queryset=Area.objects.all()),
-        name="area_list"),
-    url(r'^view/contexts$',
-        ListView.as_view(
-            queryset=Context.objects.all()),
-        name="context_list"),
-    url(r'^view/projects$',
-        ListView.as_view(
-            queryset=Project.objects.all()),
-        name="project_list"),
+    url(r'^$',              LatestActionView.as_view()),
+    url(r'^view/actions$',  ActionView.as_view(),     name="action_list"),
+    url(r'^view/areas$',    AreaView.as_view(),       name="area_list"),
+    url(r'^view/contexts$', ContextView.as_view(),    name="context_list"),
+    url(r'^view/projects$', ProjectView.as_view(),    name="project_list"),
 )
--- ptdify/web/views.py
+++ ptdify/web/views.py
@@ -1,1 +1,40 @@
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView
+from web.models import Action, Area, Context, Project
+
+class LatestActionView(ListView):
+    context_object_name='latest_action_list'
+    queryset=Action.objects.all()[:5]
+
+class ActionView(ListView):
+    def get_queryset(self):
+        return Action.objects.for_user(self.request.user)
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(ActionView, self).dispatch(*args, **kwargs)
+
+class AreaView(ListView):
+    def get_queryset(self):
+        return Area.objects.for_user(self.request.user)
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(AreaView, self).dispatch(*args, **kwargs)
+
+class ContextView(ListView):
+    def get_queryset(self):
+        return Context.objects.for_user(self.request.user)
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(ContextView, self).dispatch(*args, **kwargs)
+
+class ProjectView(ListView):
+    def get_queryset(self):
+        return Project.objects.for_user(self.request.user)
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(ProjectView, self).dispatch(*args, **kwargs)
