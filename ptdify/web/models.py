8672ac36efded5c48c0a5db5ebc1a2d696f62756
Maarten Aertsen <m.aertsen@visnescire.net>
2011-07-25 01:21:53 +0200

    Used abstract base classes to simplify model code

--- ptdify/web/models.py
+++ ptdify/web/models.py
@@ -2,45 +2,46 @@ from django.contrib.auth.models import User
 from django.db import models
 from web.managers import UserManager
 
-class Area(models.Model):
+# Abstract base classes
+class UserModel(models.Model):
     user = models.ForeignKey(User)
-    name = models.CharField(max_length=50)
 
     # Managers
     objects = UserManager()
 
+    class Meta:
+        abstract = True
+
+class AreaUserModel(UserModel):
+    area = models.ForeignKey('Area')
+
+    class Meta:
+        abstract = True
+
+# Normal classes
+class Area(UserModel):
+    name = models.CharField(max_length=50)
+
     def __unicode__(self):
         return self.name
 
-class Context(models.Model):
-    user = models.ForeignKey(User)
-    area = models.ForeignKey(Area)
+class Context(AreaUserModel):
     name = models.CharField(max_length=50)
     order = models.PositiveIntegerField(blank=True,null=True)
 
-    # Managers
-    objects = UserManager()
-
     def __unicode__(self):
         return self.name
 
-class Project(models.Model):
-    user = models.ForeignKey(User)
-    area = models.ForeignKey(Area)
+class Project(AreaUserModel):
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True)
     defaultContext = models.ForeignKey(Context)
     order = models.PositiveIntegerField(blank=True,null=True)
 
-    # Managers
-    objects = UserManager()
-
     def __unicode__(self):
         return self.name
 
-class Action(models.Model):
-    user = models.ForeignKey(User)
-    area = models.ForeignKey(Area)
+class Action(AreaUserModel):
     description = models.CharField(max_length=100)
     notes = models.TextField(blank=True)
     context = models.ForeignKey(Context)
@@ -52,9 +53,5 @@ class Action(models.Model):
     completed = models.BooleanField()
     future = models.BooleanField()
 
-    # Managers
-    objects = UserManager()
-
     def __unicode__(self):
         return self.description
-
