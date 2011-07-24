1b08e0df60b7cc656fac29f2a07b32a3248f6342
Maarten Aertsen <m.aertsen@visnescire.net>
2011-07-24 20:01:46 +0200

    Added generic views to display all standard models

--- ptdify/web/urls.py
+++ ptdify/web/urls.py
@@ -1,10 +1,26 @@
 from django.conf.urls.defaults import *
 from django.views.generic import ListView
-from web.models import Action
+from web.models import Action, Area, Context, Project
 
 urlpatterns = patterns('',
-    (r'^$',
+    url(r'^$',
         ListView.as_view(
             queryset=Action.objects.all()[:5],
             context_object_name='latest_action_list')),
+    url(r'^view/actions$',
+        ListView.as_view(
+            queryset=Action.objects.all()),
+        name="action_list"),
+    url(r'^view/areas$',
+        ListView.as_view(
+            queryset=Area.objects.all()),
+        name="area_list"),
+    url(r'^view/contexts$',
+        ListView.as_view(
+            queryset=Context.objects.all()),
+        name="context_list"),
+    url(r'^view/projects$',
+        ListView.as_view(
+            queryset=Project.objects.all()),
+        name="project_list"),
 )
