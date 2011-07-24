48f8fdc1da21d3a1eff442dc740742123affdf78
Maarten Aertsen <m.aertsen@visnescire.net>
2011-07-24 19:44:56 +0200

    Added Area model to admin

--- ptdify/web/admin.py
+++ ptdify/web/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from web.models import Action, Context, Project
+from web.models import Action, Area, Context, Project
 
 admin.site.register(Action)
+admin.site.register(Area)
 admin.site.register(Context)
 admin.site.register(Project)
