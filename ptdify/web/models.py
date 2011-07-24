e974e48e8d4adfcb8eabcc29080479638ebacd64
Maarten Aertsen <m.aertsen@visnescire.net>
2011-07-25 01:21:58 +0200

    Removed ordering from context

--- ptdify/web/models.py
+++ ptdify/web/models.py
@@ -27,7 +27,6 @@ class Area(UserModel):
 
 class Context(AreaUserModel):
     name = models.CharField(max_length=50)
-    order = models.PositiveIntegerField(blank=True,null=True)
 
     def __unicode__(self):
         return self.name
